1845d51d3fe4e27e459aacfa326a511287171326
Kevilin Domingos <kevilinmarcondes356@gmail.com>
2025-04-07 16:06:44 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import style from './App.module.css'
+import { Menu } from './components/menu'
+import  Contact from './Contact'
+
+export default function App() {
+
+  return (
+    <>
+  
+     <Menu option01='sessão 1' option02='sessão 2' option03='contato' ></Menu>
+     <main>
+      <section id='s1'>
+      <h2>sessão 1</h2>
+      </section>
+
+      <section id='s2'>
+      <h2>sessão 2</h2>
+      </section>
+
+      <section id='s3'>
+      <h2>sessão 3</h2>
+      </section>
+     </main>
+    </>
+  )
+}
---
+++ src/Contact.jsx
@@ -0,0 +1,10 @@
+import style from './Contact.module.css'
+
+function contact (){
+    return(
+        <>
+        <h1 className={style.tt}>oi</h1>
+        </>
+    )
+}
+export default contact
